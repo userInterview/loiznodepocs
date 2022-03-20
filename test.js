01336ea5e1382ed463032f7e5f950d4ad277f829
Loizani <Loizani@aol.com>
2022-03-20 18:52:23 +0100

    ES6 + eslitnt sans babel - saisie javascript en ligne de commande dans test.js

--- test.js
+++ test.js
@@ -1,23 +1,25 @@
 
 console.log("Début");
 import prompt from 'prompt';
-/*
-var valLoizasync = await (new Promise(
-       function (resolved, reject){
+
+var LoizPromise =  await (new Promise(
+function (resolved, reject){
               prompt.start();
               prompt.get(['username', 'email'], function (err, result) {
                      console.log('Command-line input received:');
                      console.log('  username: ' + result.username);
                      console.log('  email: ' + result.email);
                      resolved(result.username + " : " + result.email);                     
-              })
-       }
-)).then(function(resolved, reject) {      
-       return resolved ;
-})  ;  ;
+              }) ;
+       } ))
+              
+              
+              .then(function(resolved, reject) {      
+                     return resolved ;
+       }) ;  
 
-console.log("terminé : " + valLoizasync );
-*/
+console.log("prompt terminé : " + LoizPromise );
+
 try {
        var k = new Map();
        console.log("new Map() possible : ES6 semble actif.") ;
