1848f166680350a0e892d5b89206e9caffe25837
Loizani <Loizani@aol.com>
2022-03-21 20:35:02 +0100

    fichier test.js modifié pour montrer que l'instruction  - new Map() - avec nodejs version 0.10.0

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 console.log("coucou à 01:09");
+
+try {
+       var k = new Map();
+       console.log("ES6 supported!!")
+} catch (err) {
+       console.log("ES6 not supported :(")
+}
+
+try {
+       var k = new HashMap();
+       console.log("ES100 supported!!")
+} catch (err) {
+       console.log("ES100 not supported :(")
+}
